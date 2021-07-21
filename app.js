d8b4e605afa158914ad775c7045c464727cf3340
Luke Rogers <roger1lt@cmich.edu>
2021-07-20 20:40:24 -0500

    Finished Lesson 2 about importing files

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const fs = require('fs')
 
-//fs.writeFileSync('note.txt', 'Hello World')
-fs.appendFileSync('note.txt', 'My name is Luke Rogers')
+const getNotes = require('./notes.js')
 
 
+const myNotes = getNotes()
+
+
+console.log(myNotes)
+
+
+
+//Old code from tutorial part of lesson 
+// const add = require('./utils.js')
+
+// const sum = add(4, -2)
+
+
+// console.log(sum)
