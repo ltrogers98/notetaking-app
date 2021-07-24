d54ee3a2e60e7c538e9bc72b9bd4ef4753244d73
Luke Rogers <roger1lt@cmich.edu>
2021-07-24 15:40:30 -0500

    Added yargs and functionaility

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 //Import modules
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
+//custome yargs version
+yargs.version('1.1.0')
+
+//Set up yargs functionaility
+//Add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note')
+    }
+})
+//Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note')
+    }
+})
+//List command
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function () {
+        console.log('Listing notes')
+    }
+})
+//
+//Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
 //Get command
-const command = process.argv[2]
+//const command = process.argv[2]
+
+console.log(yargs.argv)
 
 
-//Print Command to console
-if (command === 'add') {
-        console.log('Adding note')
-} else if (command === 'remove') {
-    console.log('Removing note')
-}
